ff07bc4239d3b87d86b2e4b72df3141240a0aa97
Christian <christiancharlton17@gmail.com>
2023-03-20 10:53:58 -0600

    Add dynamic copyright

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import "./hero-img.jpg";
 import "./chef.jpg";
 import "./cocktail.jpg";
 import "./logo-mobile.png";
+
+const aDate = new Date();
+const copyYear = aDate.getFullYear();
+const copyYearSpan = document.getElementById("copy-year");
+copyYearSpan.textContent = copyYear;
