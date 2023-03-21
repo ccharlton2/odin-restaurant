0d997cf85c005edff6dda904d7c374baa75b7262
Christian <christiancharlton17@gmail.com>
2023-03-21 10:58:42 -0600

    Change logo for mobile

--- src/index.js
+++ src/index.js
@@ -9,3 +9,25 @@ const aDate = new Date();
 const copyYear = aDate.getFullYear();
 const copyYearSpan = document.getElementById("copy-year");
 copyYearSpan.textContent = copyYear;
+
+const logoImg = document.querySelector(".logo");
+console.log(logoImg);
+
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+    logoImg.src = "./logo-mobile.png";
+  } else {
+    logoImg.src = "./logo.png";
+  }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
